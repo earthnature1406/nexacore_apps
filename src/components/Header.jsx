28b5490f479def4dab53a586858e8fdfe5e25661
earthnature1406 <peepletree801@gmail.com>
2026-05-14 18:21:37 +0000

    Fix logo import paths to use src/assets/logo.jpeg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
-import logo from '../../logo.jpeg';
+import logo from '../assets/logo.jpeg';
 
 export default function Header() {
   const location = useLocation();
